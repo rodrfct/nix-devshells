3709ad53ef2b0432b0a6250dec5af9d5d6fba0ee
rodrfct <rodrigoca.fct@gmail.com>
2025-04-12 19:03:07 +0200

    Add vanilla html,css and js template

---
+++ web-vanilla/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Vue.js devShell";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          vscode-langservers-extracted
+          typescript
+          typescript-language-server
+
+          firefox-devedition
+          brave
+          epiphany
+        ];
+
+        shellHook = ''
+          exec fish
+        '';
+      };
+
+  });
+}
